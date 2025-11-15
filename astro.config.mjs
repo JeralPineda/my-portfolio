ecc5a083a41b4e2131596415b975a2aa5c3d151e
Jeral Pineda <jeral.cris@gmail.com>
2025-11-14 21:13:47 -0600

    fix: add path aliases config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import path from 'node:path'
+import { defineConfig } from 'astro/config'
+
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+})
