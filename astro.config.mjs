5fd6e33acb7bedb934f25b417758d8ec4066fac4
Jeral Pineda <jeral.cris@gmail.com>
2026-07-21 21:41:23 -0600

    fix: agregar fuente al proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Inter",
+      cssVariable: "--font-inter",
+      weights: [400, 500, 600, 700],
+      styles: ["normal"],
+    },
+  ],
   site: "https://jeralpineda.dev",
   integrations: [sitemap()],
   vite: {
